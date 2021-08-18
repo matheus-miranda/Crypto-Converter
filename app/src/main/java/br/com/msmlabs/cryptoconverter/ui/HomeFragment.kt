388e21f7b474896902e3f210952c1002a1cc831d
matheus-miranda <matheus.sm86@gmail.com>
2021-08-18 15:50:00 -0300

    Add view binding to HomeFragment

--- app/src/main/java/br/com/msmlabs/cryptoconverter/ui/HomeFragment.kt
+++ app/src/main/java/br/com/msmlabs/cryptoconverter/ui/HomeFragment.kt
@@ -6,14 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import br.com.msmlabs.cryptoconverter.R
+import br.com.msmlabs.cryptoconverter.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
