8a67c4220f29c166e38593b3d07b496e9de3c43a
matheus-miranda <matheus.sm86@gmail.com>
2021-08-19 23:42:27 -0300

    Bind custom array adapter for currencies and fix row layout

--- app/src/main/java/br/com/msmlabs/cryptoconverter/data/model/spinner/Currency.kt
+++ app/src/main/java/br/com/msmlabs/cryptoconverter/data/model/spinner/Currency.kt
@@ -3,4 +3,9 @@ package br.com.msmlabs.cryptoconverter.data.model.spinner
 /**
  * This data class represents a single row in our spinner
  */
-data class Currency(val image: Int, val text: String)
+data class Currency(val image: Int, val text: String) {
+
+    override fun toString(): String {
+        return text
+    }
+}
--- app/src/main/java/br/com/msmlabs/cryptoconverter/ui/HomeFragment.kt
+++ app/src/main/java/br/com/msmlabs/cryptoconverter/ui/HomeFragment.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
+import br.com.msmlabs.cryptoconverter.R
 import br.com.msmlabs.cryptoconverter.data.model.Crypto
 import br.com.msmlabs.cryptoconverter.data.model.Fiat
+import br.com.msmlabs.cryptoconverter.data.model.spinner.Currency
 import br.com.msmlabs.cryptoconverter.databinding.FragmentHomeBinding
+import br.com.msmlabs.cryptoconverter.presentation.adapter.CurrencyArrayAdapter
 
 class HomeFragment : Fragment() {
 
@@ -32,17 +34,39 @@ class HomeFragment : Fragment() {
      * Binds the array values to the currency drop down menus
      */
     private fun bindAdapters() {
-        // Get values from Enum Class Fiat
-        val fiatList = Fiat.values()
-        val fiatAdapter =
-            ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, fiatList)
+        // Create custom array adapter for fiat currencies
+        val fiatAdapter = CurrencyArrayAdapter(
+            requireContext(),
+            listOf(
+                Currency(R.drawable.au, Fiat.AUD.toString()),
+                Currency(R.drawable.br, Fiat.BRL.toString()),
+                Currency(R.drawable.ca, Fiat.CAD.toString()),
+                Currency(R.drawable.euro, Fiat.EUR.toString()),
+                Currency(R.drawable.gb, Fiat.GBP.toString()),
+                Currency(R.drawable.us, Fiat.USD.toString()),
+            )
+        )
 
-        // Get values from Enum Class Crypto
-        val cryptoList = Crypto.values()
-        val cryptoAdapter =
-            ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, cryptoList)
+        // Create custom array adapter for crypto currencies
+        val cryptoAdapter = CurrencyArrayAdapter(
+            requireContext(),
+            listOf(
+                Currency(R.drawable.ada_cardano, Crypto.ADA.toString()),
+                Currency(R.drawable.bch_bitcoin_cash, Crypto.BCH.toString()),
+                Currency(R.drawable.btc_bitcoin, Crypto.BTC.toString()),
+                Currency(R.drawable.dash, Crypto.DASH.toString()),
+                Currency(R.drawable.doge_dogecoin, Crypto.DOGE.toString()),
+                Currency(R.drawable.eth_ethereum, Crypto.ETH.toString()),
+                Currency(R.drawable.ltc_litecoin, Crypto.LTC.toString()),
+                Currency(R.drawable.neo, Crypto.NEO.toString()),
+                Currency(R.drawable.usdt_tether, Crypto.USDT.toString()),
+                Currency(R.drawable.xlm_stellar, Crypto.XLM.toString()),
+                Currency(R.drawable.xmr_monero, Crypto.XMR.toString()),
+                Currency(R.drawable.xrp_ripple, Crypto.XRP.toString())
+            )
+        )
 
-        // Set array list adapter to the drop down views
+        // Set array list adapter to the drop down spinner
         binding.tvConvertFrom.setAdapter(fiatAdapter)
         binding.tvConvertTo.setAdapter(cryptoAdapter)
 
