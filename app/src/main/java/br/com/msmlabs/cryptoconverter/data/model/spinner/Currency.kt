a8302817cc9fe30ad544669146189c6efd9b943c
matheus-miranda <matheus.sm86@gmail.com>
2021-08-19 22:25:14 -0300

    Create data class for drop down spinner

---
+++ app/src/main/java/br/com/msmlabs/cryptoconverter/data/model/spinner/Currency.kt
@@ -0,0 +1,6 @@
+package br.com.msmlabs.cryptoconverter.data.model.spinner
+
+/**
+ * This data class represents a single row in our spinner
+ */
+data class Currency(val image: Int, val currency: String)
